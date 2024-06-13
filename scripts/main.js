2fdeb1a98b1f48172026f25c352bc4e47edfa99b
Liana Tutt <tuttlr@rose-hulman.edu>
2024-06-13 18:12:29 -0400

    colors and new sections

--- scripts/main.js
+++ scripts/main.js
@@ -80,5 +80,67 @@ AOS.init({
     once: true,    // Whether animation should happen only once
 });
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const track = document.querySelector('.carousel-track');
+//     const slides = Array.from(track.children);
+//     const nextButton = document.querySelector('.carousel-button-right');
+//     const prevButton = document.querySelector('.carousel-button-left');
+//     const slideWidth = slides[0].getBoundingClientRect().width;
+
+//     // Arrange the slides next to each other
+//     const setSlidePosition = (slide, index) => {
+//         slide.style.left = slideWidth * index + 'px';
+//     };
+//     slides.forEach(setSlidePosition);
+
+//     const moveToSlide = (track, currentSlide, targetSlide) => {
+//         track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+//         currentSlide.classList.remove('current-slide');
+//         targetSlide.classList.add('current-slide');
+//     };
+
+//     // When I click left, move slides to the left
+//     prevButton.addEventListener('click', e => {
+//         const currentSlide = track.querySelector('.current-slide');
+//         const prevSlide = currentSlide.previousElementSibling;
+
+//         if (prevSlide) {
+//             moveToSlide(track, currentSlide, prevSlide);
+//         }
+//     });
+
+//     // When I click right, move slides to the right
+//     nextButton.addEventListener('click', e => {
+//         const currentSlide = track.querySelector('.current-slide');
+//         const nextSlide = currentSlide.nextElementSibling;
+
+//         if (nextSlide) {
+//             moveToSlide(track, currentSlide, nextSlide);
+//         }
+//     });
+
+//     // Set the first slide as the current slide
+//     slides[0].classList.add('current-slide');
+// });
+$(document).ready(function(){
+    $('.achievements-carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true, // Enables navigation dots
+        arrows: true, // Enables navigation arrows
+        prevArrow: '.carousel-button-left',
+        nextArrow: '.carousel-button-right',
+        autoplay: true, // Automatically cycles through slides
+        autoplaySpeed: 5000, // Time each slide is shown in milliseconds
+    });
+});
+
+
+
+
+
+
+
 
 
